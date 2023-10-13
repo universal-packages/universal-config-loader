d8a8fc85d949769697992503ac1b3ba5d0025dc7
David De Anda <omarandstuff@gmail.com>
2023-10-13 01:24:58 -0600

    Upgrade libraries

--- tests/loadFileConfig.test.ts
+++ tests/loadFileConfig.test.ts
@@ -105,7 +105,7 @@ describe(loadFileConfig, (): void => {
       error = err
     }
 
-    expect(error.message).toEqual(`Unexpected token $ in JSON at position 0; in file "${path.resolve('./tests/__fixtures__/bad/json/error.json')}"`)
+    expect(error.message).toEqual(`Unexpected token '$', \"$\" is not valid JSON; in file "${path.resolve('./tests/__fixtures__/bad/json/error.json')}"`)
 
     try {
       await loadFileConfig('./tests/__fixtures__/bad/yaml/error')
